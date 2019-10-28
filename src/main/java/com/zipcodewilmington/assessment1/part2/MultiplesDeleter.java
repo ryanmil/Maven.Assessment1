41c5f2d1f25b81d5b0ae4e01e3aef81d27e39908
Ryan <ryan@rmiller.dev>
2019-10-28 10:51:59 -0400

    Write MultipleDeleter methods

--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -10,7 +10,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
-        return null;
+        return deleteMultiplesOfN(ints, 2);
     }
 
     /**
@@ -19,7 +19,20 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with odds removed
      */
     public Integer[] deleteOdds(Integer[] ints) {
-        return null;
+        Integer count = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if(ints[i] % 2 != 0) {
+                count++;
+            }
+        }
+        Integer[] resized = new Integer[ints.length - count];
+        for (int i = 0, j = 0; i < ints.length; i++) {
+            if(ints[i] % 2 == 0) {
+                resized[j] = ints[i];
+                j++;
+            }
+        }
+        return resized;
     }
 
     /**
@@ -28,7 +41,7 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by 3 removed
      */
     public Integer[] deleteMultiplesOf3(Integer[] ints) {
-        return null;
+        return deleteMultiplesOfN(ints, 3);
     }
 
     /**
@@ -38,6 +51,19 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with numbers indivisible by `multiple` removed
      */
     public Integer[] deleteMultiplesOfN(Integer[] ints, int multiple) {
-        return null;
+        Integer count = 0;
+        for (int i = 0; i < ints.length; i++) {
+            if(ints[i] % multiple == 0) {
+                count++;
+            }
+        }
+        Integer[] resized = new Integer[ints.length - count];
+        for (int i = 0, j = 0; i < ints.length; i++) {
+            if(ints[i] % multiple != 0) {
+                resized[j] = ints[i];
+                j++;
+            }
+        }
+        return resized;
     }
 }
