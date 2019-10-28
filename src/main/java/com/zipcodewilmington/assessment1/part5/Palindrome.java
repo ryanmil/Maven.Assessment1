f037abd289a1df765a805571faa7616e671e0664
Ryan <ryan@rmiller.dev>
2019-10-28 12:03:21 -0400

    Write palindrom method

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,19 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Integer count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                String str = input.substring(i, j);
+                if (str.equals(BasicStringUtils.reverse(str))) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
